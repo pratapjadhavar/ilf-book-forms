ba4db446e9766e06f35a2c4f52d165588cce32ca
Dan Kennedy <dannykennedy@email.com>
2020-01-31 14:18:03 +1100

    communities added to list with duplicate checking and X icons

--- communitySearch.js
+++ communitySearch.js
@@ -34,7 +34,20 @@ var communities = [
 var i;
 for (i = 0; i < communities.length; i++) {
     comm = communities[i];
-    item = makeDropdownItem(comm.name, comm.state, comm.alternativeNames);
+    var item = makeDropdownItem(
+        comm.name,
+        comm.state,
+        comm.alternativeNames,
+        i
+    );
+    item.addEventListener("click", function(event) {
+        var commName =
+            communities[event.currentTarget.getAttribute("data-index")].name;
+        var commState =
+            communities[event.currentTarget.getAttribute("data-index")].state;
+
+        addCommunityTagToList(commName, commState);
+    });
     addDropdownItem(item);
 }
 
@@ -42,73 +55,13 @@ function addDropdownItem(item) {
     document.getElementById("dropdown-items").appendChild(item);
 }
 
-function makeDropdownItem(name, stateOrTerritory, alternativeNames) {
-    item = document.createElement("div");
-    item.classList = "dropdown-item bbox";
-    // Name
-    var nameSpan = makeNameDiv(name, stateOrTerritory);
-    item.appendChild(nameSpan);
-
-    if (!(alternativeNames == "")) {
-        var alternativeNamesDiv = makeAlternativeNameDiv(alternativeNames);
-        item.appendChild(alternativeNamesDiv);
-    }
-    return item;
-}
-
-function makeNameDiv(name, stateOrTerritory) {
-    var nameDiv = document.createElement("div");
-    var nameSpan = document.createElement("span");
-    nameSpan.innerText = name;
-    nameSpan.classList = "dropdown-name-span";
-    nameDiv.appendChild(nameSpan);
-
-    if (!(stateOrTerritory === "")) {
-        var stateSpan = document.createElement("span");
-        stateSpan.classList = "dropdown-state-span";
-        stateSpan.innerText = ", " + stateOrTerritory;
-        nameDiv.appendChild(stateSpan);
-    }
-    return nameDiv;
-}
-
-function makeAlternativeNameDiv(alternativeNames) {
-    var alternativeNamesDiv = document.createElement("div");
-    var alternativeNamesSpan = document.createElement("span");
-    var alsoKnownAsSpan = document.createElement("span");
-    alternativeNamesSpan.innerText = alternativeNames;
-    alsoKnownAsSpan.innerText = "Also known as: ";
-    alternativeNamesSpan.classList = "dropdown-alt-names-span";
-    alsoKnownAsSpan.classList = "dropdown-also-known-span";
-    alternativeNamesDiv.classList = "dropdown-alt-names-div";
-    alternativeNamesDiv.appendChild(alsoKnownAsSpan);
-    alternativeNamesDiv.appendChild(alternativeNamesSpan);
-    console.log(alternativeNamesDiv);
-
-    return alternativeNamesDiv;
-}
-
 // Add communities to dropdown
 
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("dropdown-content").classList.toggle("show");
-}
-
 function showDropdown() {
-    document.getElementById("dropdown-items").style.display = "block";
+    showElementById("dropdown-items");
 }
 function hideDropdown() {
-    document.getElementById("dropdown-items").style.display = "none";
-}
-
-function showElementById(id) {
-    document.getElementById(id).style.display = "block";
-}
-
-function hideElementById(id) {
-    document.getElementById(id).style.display = "none";
+    hideElementById("dropdown-items");
 }
 
 function filterFunction() {
@@ -127,8 +80,7 @@ function filterFunction() {
     a = div.querySelectorAll(".dropdown-item");
     for (i = 0; i < a.length; i++) {
         var altName = "";
-        commName = a[i].getElementsByClassName("dropdown-name-span")[0]
-            .innerText;
+        commName = a[i].getElementsByClassName("name-span")[0].innerText;
 
         if (a[i].getElementsByClassName("dropdown-alt-names-span").length > 0) {
             altName = a[i].getElementsByClassName("dropdown-alt-names-span")[0]
@@ -145,13 +97,33 @@ function filterFunction() {
     }
 }
 
-function makeTag(text) {
-    var tag = document.createElement("div");
-    var label = document.createElement("span");
-    tag.classList = "chosen-community-tag";
-    label.innerText = text;
-    tag.appendChild(label);
-    return tag;
+function tagExistsInList(name) {
+    var existingNames = document
+        .getElementById("community-selection-chosen-area")
+        .getElementsByClassName("name-span");
+    console.log("nmz", existingNames);
+    var i;
+    var exists = false;
+    for (i = 0; i < existingNames.length; i++) {
+        console.log("nm", existingNames[i].innerText);
+
+        if (existingNames[i].innerText == name) {
+            exists = true;
+            break;
+        }
+    }
+    console.log("it exists", exists);
+    return exists;
+}
+
+function addCommunityTagToList(name, state) {
+    if (tagExistsInList(name)) {
+        alert("Community is already in the list");
+        return false;
+    }
+    var insideHTML = makeNameDiv(name, state);
+    var tag = makeTag(insideHTML);
+    addCommunityTag(tag);
 }
 
 function addCommunityTag(tag) {
@@ -174,3 +146,87 @@ document
             // Make the selection go up
         }
     });
+
+///////////////////////////
+// HTML ELEMENT CREATION //
+///////////////////////////
+
+function makeTag(nameDiv) {
+    var tag = document.createElement("div");
+
+    tag.classList = "chosen-community-tag bbox";
+    tag.appendChild(nameDiv);
+    var x = makeRemoveTagButton();
+    tag.appendChild(x);
+    return tag;
+}
+
+function makeRemoveTagButton() {
+    var x = document.createElement("div");
+    x.classList = "remove-tag-icon";
+    var icon = document.createElement("i");
+    icon.classList = "fa fa-times";
+    x.appendChild(icon);
+    x.addEventListener("click", function() {
+        alert("removing the tag!");
+    });
+    return x;
+}
+
+function makeDropdownItem(name, stateOrTerritory, alternativeNames, index) {
+    item = document.createElement("div");
+    item.classList = "dropdown-item bbox";
+    item.setAttribute("data-index", index);
+    // Name
+    var nameSpan = makeNameDiv(name, stateOrTerritory);
+    item.appendChild(nameSpan);
+
+    if (!(alternativeNames == "")) {
+        var alternativeNamesDiv = makeAlternativeNameDiv(alternativeNames);
+        item.appendChild(alternativeNamesDiv);
+    }
+    return item;
+}
+
+function makeNameDiv(name, stateOrTerritory) {
+    var nameDiv = document.createElement("div");
+    nameDiv.classList = "name-div";
+    var nameSpan = document.createElement("span");
+    nameSpan.innerText = name;
+    nameSpan.classList = "name-span";
+    nameDiv.appendChild(nameSpan);
+
+    if (!(stateOrTerritory === "")) {
+        var stateSpan = document.createElement("span");
+        stateSpan.classList = "state-span";
+        stateSpan.innerText = ", " + stateOrTerritory;
+        nameDiv.appendChild(stateSpan);
+    }
+    return nameDiv;
+}
+
+function makeAlternativeNameDiv(alternativeNames) {
+    var alternativeNamesDiv = document.createElement("div");
+    var alternativeNamesSpan = document.createElement("span");
+    var alsoKnownAsSpan = document.createElement("span");
+    alternativeNamesSpan.innerText = alternativeNames;
+    alsoKnownAsSpan.innerText = "Also known as: ";
+    alternativeNamesSpan.classList = "dropdown-alt-names-span";
+    alsoKnownAsSpan.classList = "dropdown-also-known-span";
+    alternativeNamesDiv.classList = "dropdown-alt-names-div";
+    alternativeNamesDiv.appendChild(alsoKnownAsSpan);
+    alternativeNamesDiv.appendChild(alternativeNamesSpan);
+    return alternativeNamesDiv;
+}
+
+//////////////////////
+// HELPER FUNCTIONS //
+//////////////////////
+
+function showElementById(id) {
+    document.getElementById(id).style.display = "block";
+}
+
+function hideElementById(id) {
+    document.getElementById(id).style.display = "none";
+}
