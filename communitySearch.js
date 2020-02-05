92830601abfb8b411fd6a10890e5f79b2185d2e9
Dan Kennedy <dannykennedy@email.com>
2020-02-06 09:58:46 +1100

    field warnings, fixes for community selector

--- communitySearch.js
+++ communitySearch.js
@@ -1,3 +1,6 @@
+// Selected community tags will have their 'data-id' attribute set to one of the ids in this list
+// If they're not in the list, the attribute will be unset.
+
 var communities = [
     {
         name: "Gunbalanya (Oenpelli)",
@@ -71,8 +74,7 @@ function addTagListener(event) {
             communities[event.currentTarget.getAttribute("data-index")].name;
         commState =
             communities[event.currentTarget.getAttribute("data-index")].state;
-        commId =
-            communities[event.currentTarget.getAttribute("data-index")].state;
+        commId = communities[event.currentTarget.getAttribute("data-index")].id;
     }
 
     addCommunityTagToList(commName, commState, commId);
@@ -83,32 +85,23 @@ function addTagListener(event) {
 // Filter the dropdown list based on user input
 function filterFunction(event) {
     // Ignore non-character keys
-
     if (
         isEnterEvent(event) ||
         isArrowUpEvent(event) ||
-        isArrowDownEvent(event)
+        isArrowDownEvent(event) ||
+        isShiftEvent(event)
     ) {
         return;
     }
-
-    // console.log("in here", event);
 
     var input = event.currentTarget;
     var val = input.value;
     var filter = val.toUpperCase();
 
-    // console.log("filter: ", filter);
-
-    console.log("length", filter.length);
-
     if (filter.length < 1) {
         hideDropdown();
-        hideElementById("dropdown-items-noresults");
-        // console.log("hiding dropdown!");
     } else {
         showDropdown();
-        // console.log("showing dropdown!");
     }
 
     var shownItemIndex = 0;
@@ -120,22 +113,25 @@ function filterFunction(event) {
 
     var i;
     var found = false;
-    for (i = 0; i < communities.length; i++) {
-        var community = communities[i] || {};
-        var commName = community.name || "";
-        var altName = community.alternativeNames || "";
+    for (i = 0; i < dropdownItems.length; i++) {
+        // var community = communities[i] || {};
+        var commName = dropdownItems[i].querySelector(".name-span").innerText;
+        var altName = "";
+        if (dropdownItems[i].querySelector(".dropdown-alt-names-span")) {
+            altName = dropdownItems[i].querySelector(".dropdown-alt-names-span")
+                .innerText;
+        }
         var txtValue = commName + " " + altName;
 
-        if (txtValue.toUpperCase().includes(filter)) {
+        // "Add new community"
+        // if ()
+
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
             dropdownItems[i].style.display = "";
             dropdownItems[i].setAttribute("shown-item-index", shownItemIndex);
             shownItemIndex++;
             found = true;
         } else {
-            // console.log("didn't match");
-            // console.log("txtval", txtValue);
-            // console.log("filter", filter);
-
             dropdownItems[i].style.display = "none";
             dropdownItems[i].removeAttribute("shown-item-index");
         }
@@ -147,12 +143,36 @@ function filterFunction(event) {
 
 // Enter key or arrow in search bar
 getId("ilf-search-bar").addEventListener("keydown", function(event) {
+    var searchBar = document.getElementById("ilf-search-bar");
     if (isEnterEvent(event)) {
-        searchBar = document.getElementById("ilf-search-bar");
-        text = searchBar.value;
-        if (text == "") return; // No empty tags
-        addCommunityTagToList(text, "");
+        // Check first to see if an item is selected
+        var selectedItems = document.querySelectorAll(
+            ".dropdown-item.selected"
+        );
+        if (selectedItems.length && dropdownIsOpen()) {
+            var selectedItem = selectedItems[0];
+            var commName = selectedItem.querySelectorAll(".name-span")[0]
+                .innerText;
+            var state = selectedItem.querySelectorAll(".state-span");
+            var commState = state.length
+                ? state[0].innerText.replace(", ", "")
+                : "";
+            var id = selectedItem.getAttribute("data-id");
+            var commId = id ? id : "";
+            addCommunityTagToList(commName, commState, commId);
+        } else {
+            text = searchBar.value;
+            if (text == "") return; // No empty tags
+            // Try to find an id
+            var match = findCommunityInList(text, communities);
+            addCommunityTagToList(
+                match.name || text,
+                match.state || "",
+                match.id || ""
+            );
+        }
         searchBar.value = "";
+        hideDropdown();
     } else if (isArrowDownEvent(event)) {
         // Make the selection go down
         selectNextItemInDropdown("down");
@@ -170,6 +190,20 @@ function getTagsInCommunityList() {
     return getId("community-selection-chosen-area").getElementsByClassName(
         "chosen-community-tag"
     );
+}
+
+function getNumTagsNotInDatabase() {
+    var num = 0;
+    var commTags = getId(
+        "community-selection-chosen-area"
+    ).getElementsByClassName("chosen-community-tag");
+    var i;
+    for (i = 0; i < commTags.length; i++) {
+        if (!commTags[i].getAttribute("data-id")) {
+            num++;
+        }
+    }
+    return num;
 }
 
 function tagExistsInList(name) {
@@ -199,23 +233,37 @@ function numCommunitiesMessage(numComms) {
     }
 }
 
-function addCommunityTagToList(name, state, id) {
-    console.log("name", name);
-    console.log("state", state);
-    console.log("id", id);
+function notInDbMessage(numNotInDb) {
+    var baseMsg =
+        " not listed in our database, but we’ll check the details and get back to you.";
+    if (numNotInDb < 1) {
+        return "";
+    } else {
+        return numNotInDb === 1
+            ? "1 selected community is" + baseMsg
+            : numNotInDb + " selected communities are" + baseMsg;
+    }
+}
 
+function addCommunityTagToList(name, state, id) {
     if (tagExistsInList(name)) {
         alert("Community is already in the list");
         return false;
     }
     var insideHTML = makeNameDiv(name, state);
     var tag = makeTag(insideHTML);
-    tag.setAttribute("data-id", id);
+    tag.setAttribute("data-id", id || "");
     addCommunityTag(tag);
 
-    // Update message
+    // Update messages
+    recalculateTagMessages();
+}
+
+function recalculateTagMessages() {
     var numComms = getId("num-communities-selected");
     numComms.innerText = numCommunitiesMessage(getTagsInCommunityList().length);
+    var notInDbMsg = getId("not-in-database");
+    notInDbMsg.innerText = notInDbMessage(getNumTagsNotInDatabase());
 }
 
 function addCommunityTag(tag) {
@@ -250,9 +298,7 @@ function deselectDropdownItemByIndex(index) {
 
 function getShowingDropdownItems() {
     var ret = [];
-    var dropdownItems = getId("dropdown-items").getElementsByClassName(
-        "dropdown-item"
-    );
+    var dropdownItems = getAllDropdownItems();
     var i;
     for (i = 0; i < dropdownItems.length; i++) {
         if (dropdownItems[i].hasAttribute("shown-item-index")) {
@@ -260,6 +306,18 @@ function getShowingDropdownItems() {
         }
     }
     return ret;
+}
+
+function getAllDropdownItems() {
+    return getId("dropdown-items").getElementsByClassName("dropdown-item");
+}
+
+function unselectAllDropdownItems() {
+    var dropdownItems = getAllDropdownItems();
+    var i;
+    for (i = 0; i < dropdownItems.length; i++) {
+        dropdownItems[i].classList.remove("selected");
+    }
 }
 
 function getDropdownItemByIndex(index) {
@@ -303,10 +361,10 @@ function selectNextItemInDropdown(direction) {
 
 function getTextFromDropdownItem(dropdownItem) {
     var ret = "";
-    if (dropdownItem.getElementsByClassName("name-span")) {
+    if (dropdownItem.getElementsByClassName("name-span").length) {
         ret += dropdownItem.getElementsByClassName("name-span")[0].innerText;
     }
-    if (dropdownItem.getElementsByClassName("state-span")) {
+    if (dropdownItem.getElementsByClassName("state-span").length) {
         ret += dropdownItem.getElementsByClassName("state-span")[0].innerText;
     }
     return ret;
@@ -327,8 +385,6 @@ document.addEventListener("click", function(event) {
     }
 });
 
-console.log("I' here");
-
 // Add keyup listener
 getId("ilf-search-bar").addEventListener("keyup", function(event) {
     filterFunction(event);
@@ -340,7 +396,6 @@ getId("ilf-search-bar").addEventListener("keyup", function(event) {
 
 function makeTag(nameDiv) {
     var tag = document.createElement("div");
-
     tag.classList = "chosen-community-tag bbox";
     tag.appendChild(nameDiv);
     var x = makeRemoveTagButton();
@@ -358,10 +413,7 @@ function makeRemoveTagButton() {
         var list = document.getElementById("community-selection-chosen-area");
         list.removeChild(this.parentElement);
         // Update message
-        var numComms = getId("num-communities-selected");
-        numComms.innerText = numCommunitiesMessage(
-            getTagsInCommunityList().length
-        );
+        recalculateTagMessages();
     });
     return x;
 }
@@ -453,12 +505,17 @@ function isArrowDownEvent(event) {
     return event.which == 40 || event.code == "ArrowDown";
 }
 
+function isShiftEvent(event) {
+    return event.which == 16 || event.code == "Shift";
+}
+
 function showDropdown() {
     showElementById("dropdown-items");
 }
 function hideDropdown() {
     hideElementById("dropdown-items");
     hideElementById("dropdown-items-noresults");
+    unselectAllDropdownItems();
 }
 
 function clearSearchInput() {
@@ -467,4 +524,42 @@ function clearSearchInput() {
 
 function addDropdownItem(item) {
     document.getElementById("dropdown-items").appendChild(item);
+}
+
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
+
+// Attempt to guess what the user means if they press "Enter" without anything selected
+function findCommunityInList(communityName, communityList) {
+    var commName = communityName
+        .split(",")[0]
+        .trim()
+        .toUpperCase();
+
+    var i;
+    for (i = 0; i < communityList.length; i++) {
+        var rawCommunityName = communityList[i].name;
+        var nameOne = rawCommunityName
+            .split("(")[0]
+            .trim()
+            .toUpperCase();
+
+        var regexp = /.*\((.*)\)/g;
+        var match = regexp.exec(rawCommunityName);
+        var nameTwo = "Some unrealistic string";
+        if (match) {
+            nameTwo = match[1].toUpperCase();
+        }
+
+        if (
+            commName.indexOf(nameOne) === 0 ||
+            commName.indexOf(nameTwo) === 0
+        ) {
+            return communityList[i];
+        }
+    }
+    return -1;
 }
---
+++ pagecontroller.js
@@ -0,0 +1,257 @@
+/*
+ * @Purpose			: JS Controller for ILF Donation page
+ * @Author			: Rabaab (Dreamwares)
+ * @Created Date	: 02 - Nov - 2018
+ */
+
+var isDonateOnce, isIndividual; //Records donation type
+var amount; //Records the amount
+var donationType, paymentMethod;
+var firstName = "",
+    lastName = "",
+    address = "",
+    email = "",
+    phone = "",
+    orgName = "";
+var donationData = new Object();
+var paymentDetails = new Object();
+var paypalPaymentDetails = new Object();
+var creditCardDetails = new Object();
+
+$(document).ready(function() {
+    //Initializing the page
+    // doInit();
+
+    //--- Validating inputs ---//
+    $("#FirstName").blur(function() {
+        if (
+            $(this)
+                .val()
+                .trim()
+                .match("[a-zA-Z]{3,88}$")
+        ) {
+            firstName = $(this)
+                .val()
+                .trim();
+            $("#FirstName").val(firstName);
+            $("#FirstNameError").hide();
+        } else {
+            firstName = "";
+            $("#FirstNameError").show();
+        }
+    });
+
+    $("#LastName").blur(function() {
+        if (
+            $(this)
+                .val()
+                .trim()
+                .match("[a-zA-Z]{3,88}$")
+        ) {
+            lastName = $(this)
+                .val()
+                .trim();
+            $("#LastName").val(lastName);
+            $("#LastNameError").hide();
+        } else {
+            lastName = "";
+            $("#LastNameError").show();
+        }
+    });
+
+    $("#Address").blur(function() {
+        address = $(this)
+            .val()
+            .trim();
+    });
+
+    $("#OrganizationName").blur(function() {
+        if (isRequiredMissing("OrganizationName")) {
+            orgName = "";
+            $("#OrganizationNameError").show();
+        } else {
+            orgName = $(this).val();
+            $("#OrganizationNameError").hide();
+        }
+    });
+    $("#Email").blur(function() {
+        if (validateEmail("Email")) {
+            email = $(this)
+                .val()
+                .trim();
+            $("#Email").val(email);
+            $("#emailAddressError").hide();
+        } else {
+            email = "";
+            $("#emailAddressError").show();
+        }
+    });
+
+    $("#PhoneNumber").blur(function() {
+        if (validatePhone("PhoneNumber")) {
+            phone = $(this)
+                .val()
+                .trim();
+            $("#PhoneNumber").val(phone);
+            $("#phoneNumberError").hide();
+        } else {
+            phone = "";
+            $("#phoneNumberError").show();
+        }
+    });
+
+    //--- Validation Ends ---//
+});
+
+/*
+ * @Purpose	: Checks if required field missing
+ */
+function isRequiredMissing(elementName) {
+    var val = $("#" + elementName)
+        .val()
+        .trim();
+    if (val === "") {
+        return true;
+    }
+    return false;
+}
+
+/*
+ * @Purpose	: Validates email
+ */
+function validateEmail(Email) {
+    var a = $("#" + Email)
+        .val()
+        .trim();
+    var filter = /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9]+[a-zA-Z0-9.-]+[a-zA-Z0-9]+.[a-z ]{1,4}$/;
+    if (filter.test(a)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/*
+ * @Purpose	: Validates phone number
+ */
+function validatePhone(Phone) {
+    var a = $("#" + Phone)
+        .val()
+        .trim();
+    var filter = /^[0-9-+ ]+$/;
+    if (filter.test(a)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/*
+ * @Purpose	: Show / hide button
+ */
+function EnableDisableEFT() {
+    if (isDonateOnce && !isIndividual) {
+        //$('#EFTChequePaymentMode').removeAttr('disabled');
+        $("#EFTChequePaymentMode").show();
+    } else {
+        $("#EFTChequePaymentMode").hide();
+        //$('#EFTChequePaymentMode').attr('disabled','disabled');
+    }
+}
+
+function htmlDecode(value) {
+    return $("<div/>")
+        .html(value)
+        .text();
+}
+
+/*
+ * Selects Credit card payment mode
+ */
+function selectCreditCard() {
+    //Make credit card option selected
+    paymentMethod = "Credit Card";
+    $(".payment-mode").removeClass("active");
+    $("#creditCardPaymentMode").addClass("active");
+
+    //Show credit card details
+    $(".paymentMode").hide();
+    $("#credit-card-details").show();
+    $("#submitDonationDiv").show();
+}
+
+function validate() {
+    var reCaptchaResponse = grecaptcha.getResponse();
+
+    if (
+        firstName === "" ||
+        lastName === "" ||
+        email === "" ||
+        (!isIndividual && orgName === "") ||
+        phone === ""
+    ) {
+        showDetailsErrors();
+        scrollTo("custom", 0, 0);
+        $.toast({
+            heading: "Error",
+            text: "Please fill all the required fields",
+            position: "top-center",
+            icon: "error",
+        });
+        return false;
+    } else if (
+        paymentMethod == "Credit Card" &&
+        (creditCardDetails.nameOnCard == null ||
+            creditCardDetails.cardNumber == null ||
+            creditCardDetails.cvv == null ||
+            creditCardDetails.expiryMonth == null ||
+            creditCardDetails.expiryYear == null)
+    ) {
+        showCardDetailsErrors();
+        $.toast({
+            heading: "Error",
+            text: "Please provide all credit card details",
+            position: "top-center",
+            icon: "error",
+        });
+        return false;
+    } /*else if(paymentMethod == 'PayPal' && !paypalPaymentDetails.paymentDone){
+        $.toast({
+            heading: 'Error',
+            text: 'Please make payment using PayPal Checkout option',
+            position: 'top-center',
+            icon: 'error'
+        })
+        return false;
+    }*/ else if (
+        reCaptchaResponse.length == 0
+    ) {
+        $.toast({
+            heading: "Error",
+            text: "Validate captcha!",
+            position: "top-center",
+            icon: "error",
+        });
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function showDetailsErrors() {
+    if (firstName === "") {
+        $("#FirstNameError").show();
+    }
+    if (lastName === "") {
+        $("#LastNameError").show();
+    }
+    if (email === "") {
+        $("#emailAddressError").show();
+    }
+    if (!isIndividual && orgName === "") {
+        $("#OrganizationNameError").show();
+    }
+    if (phone === "") {
+        $("#phoneNumberError").show();
+    }
+}
